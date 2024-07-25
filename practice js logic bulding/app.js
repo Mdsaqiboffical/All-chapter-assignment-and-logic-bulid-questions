34909c24bc627e4886f36e0a51d4b991a425f1cf
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-07-26 01:36:42 +0500

    done

---
+++ practice js logic bulding/app.js
@@ -0,0 +1,93 @@
+// // console.log("hello world");
+
+// // var userName = "Muhammad Saqib";
+// // // 
+// // var titled = "Hello World";
+// // alert(titled)
+
+// // chapter 10
+
+// // Q1
+// A = 65;
+// Z = 90;
+// a = 45;
+// z= 69;
+
+// if(A==65 || a==45){
+//     console.log("is uppercase");
+// } else if (Z==90 || z==69){
+//     console.log("is lower case");
+// }
+
+
+// chapter 12 q:2
+
+// let num1 = +prompt("Enter First Integer")
+// let num2 = +prompt("Enter second Integer")
+
+// if (num1 > num2) {
+//     console.log(num1 + " Is Larger Integer");
+// } else if (num1 < num2){
+//     console.log(num2 + " Is Larger Integer");
+// }
+
+// q3
+
+// let userName = +prompt("Enter A Number")
+
+// if (userName == 0) {
+//     console.log(userName + " Given Number Is zero");
+// } else if (userName > 0){
+//     console.log(userName + " Given Number positive");
+// } else if (userName<0){
+//     console.log(userName + " Given Number Negative");
+// }
+
+// q4
+
+// let newNum = prompt("Enter a vowel")
+// if (newNum == "a" || newNum == "e" || newNum == "i" || newNum == "o" || newNum == "u") {
+//     alert(newNum + " is vowel")
+// } else {
+//     alert(newNum+ " Is not a vowel")
+// }
+
+// q5
+
+// let user1 = prompt("password")
+// let user2 = prompt("conform password")
+
+// if(user1 == "" || user2 == ""){
+//     alert("Please Enter a Password")
+// } else if(user1===user2){
+//     alert("Correct password")
+// } else if (user1!== user2){
+//     alert("Incorrect password")
+// }
+
+
+// q6
+// find the error
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";
+}
+else {
+    greeting = "Good evening";
+}
+
+// q7
+
+// let time = prompt("Enter time in 24-hour format (HHMM):");
+
+// if (time < 6) {
+//   console.log("Good night");
+// } else if (time < 12) {
+//   console.log("Good morning");
+// } else if (time < 18) {
+//   console.log("Good afternoon");
+// } else {
+//   console.log("Syntex error");
+// }
+
